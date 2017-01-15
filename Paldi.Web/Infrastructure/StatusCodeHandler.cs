4a1c81a1d0ce78b44dae6a0e88e7ce3f4f055745
Yauheni Khvaliuk <hvaluk@gmail.com>
2017-01-15 10:15:13 +0100

    add not found handler

---
+++ Paldi.Web/Infrastructure/StatusCodeHandler.cs
@@ -0,0 +1,29 @@
+﻿using Nancy;
+using Nancy.ErrorHandling;
+using Nancy.ViewEngines;
+
+namespace Paldi.Web.Infrastructure
+{
+    public class StatusCodeHandler : IStatusCodeHandler
+    {
+        private readonly IViewRenderer viewRenderer;
+
+        public StatusCodeHandler(IViewRenderer viewRenderer)
+        {
+            this.viewRenderer = viewRenderer;
+        }
+
+        public bool HandlesStatusCode(HttpStatusCode statusCode, NancyContext context)
+        {
+            return statusCode == HttpStatusCode.NotFound;
+        }
+
+        public void Handle(HttpStatusCode statusCode, NancyContext context)
+        {
+            var response = viewRenderer.RenderView(context, "404");
+            response.StatusCode = statusCode;
+            context.Response = response;
+            context.ViewBag.Title = "Страница не найдена";
+        }
+    }
+}
