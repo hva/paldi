8225e686baea67af0a1d63bd282f2f900cc6076f
Yauheni Khvaliuk <hvaluk@gmail.com>
2017-02-26 12:46:52 +0100

    start catalog

---
+++ Paldi.Web/Modules/CatalogModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Nancy;
+using Paldi.Web.Models;
+
+namespace Paldi.Web.Modules
+{
+    public class CatalogModule : NancyModule
+    {
+        public CatalogModule(Func<NavigationModel> createModel) : base("catalog")
+        {
+            Get["/"] = _ => View["Index.sshtml", createModel().Extend(Context)];
+
+            Get["/{section}"] = _ => View["Index.sshtml", createModel().Extend(Context)];
+        }
+    }
+}
