c806a1548f54fe537e053e57533dacca628c308c
Yauheni Khvaliuk <hvaluk@gmail.com>
2017-02-26 16:46:30 +0100

    add migrations

---
+++ Paldi.Web.Migrations/Users.cs
@@ -0,0 +1,22 @@
+﻿using FluentMigrator;
+
+namespace Paldi.Web.Migrations
+{
+    [Migration(201702261458)]
+    public class Users : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("users")
+                .WithColumn("guid").AsString(36)
+                .WithColumn("login").AsString(32)
+                .WithColumn("hash").AsString(64)
+            ;
+        }
+
+        public override void Down()
+        {
+            Delete.Table("users");
+        }
+    }
+}
